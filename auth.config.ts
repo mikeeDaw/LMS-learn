b5333c065e614e562c2abe26f4a250f852968d23
mikeeDaw <michaeldao05@gmail.com>
2024-06-26 00:45:53 +0800

    deploy fix

--- auth.config.ts
+++ auth.config.ts
@@ -20,7 +20,7 @@ export default {
           const { email, password } = validated.data;
 
           const resp = await fetch(
-            `https://learnflix-3fpjkkytm-mikeedaws-projects.vercel.app/api/mongoose/findUser`,
+            `learnflix-git-smscreen-mikeedaws-projects.vercel.app/api/mongoose/findUser`,
             {
               method: "POST",
               body: JSON.stringify({ email: email }),
